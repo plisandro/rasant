6bdb873358d3eea975db9458849dc2212e9a9409
Lisandro Pin <lisandrop@proton.me>
2026-04-03 05:43:46 +0200

    Fix crate metadata and license details.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+//#![deny(missing_docs)]
+#![allow(dead_code)]
+
 mod attributes;
 mod console;
 mod level;
@@ -6,9 +9,7 @@ mod macros;
 mod queue;
 pub mod sink;
 
-//#![deny(missing_docs)]
-//#![allow(dead_code)]
-
+// Public exported symbols
 pub use attributes::value::ToValue;
 pub use level::Level;
 pub use logger::Logger;
