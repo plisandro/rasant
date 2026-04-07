ee5bfadb032f9233d8b8bdc08af7a3525850be94
Lisandro Pin <lisandrop@proton.me>
2026-04-07 20:33:39 +0200

    Fix bug in async handler thread management.
    We were not properly dropping the shared sender channel, so the
    async handler thread was never properly shutting down when no
    async logers were left.
    
    This code really needs optimization, but now it's at least WAI.

--- src/queue.rs
+++ src/queue.rs
@@ -20,9 +20,9 @@ enum AsyncSinkOp {
 }
 
 struct AsyncSinkHandler {
-	tx: mpsc::Sender<AsyncSinkOp>,
+	tx: Option<mpsc::Sender<AsyncSinkOp>>,
 	rx_handler: Option<thread::JoinHandle<()>>,
-	size: Arc<Mutex<usize>>,
+	queue_size: Arc<Mutex<usize>>,
 }
 
 impl AsyncSinkHandler {
@@ -63,19 +63,27 @@ impl AsyncSinkHandler {
 		});
 
 		Self {
-			tx: tx,
+			tx: Some(tx),
 			rx_handler: Some(rx_handler),
-			size: size,
+			queue_size: size,
 		}
 	}
 
-	fn queue_size(&self) -> usize {
-		*(self.size.lock().unwrap())
+	fn get_queue_size(&self) -> usize {
+		*(self.queue_size.lock().unwrap())
+	}
+
+	fn inc_queue_size(&self) {
+		*(self.queue_size.lock().unwrap()) += 1;
 	}
 
 	fn log(&self, sink: SinkRef, update: LogUpdate, attrs: attributes::Map) {
-		*(self.size.lock().unwrap()) += 1;
-		match self.tx.send(AsyncSinkOp::Log {
+		let Some(ref tx) = self.tx else {
+			return;
+		};
+		self.inc_queue_size();
+
+		match tx.send(AsyncSinkOp::Log {
 			sink: sink,
 			update: update,
 			attrs: attrs,
@@ -86,8 +94,12 @@ impl AsyncSinkHandler {
 	}
 
 	fn flush_sink(&self, sink: SinkRef) {
-		*(self.size.lock().unwrap()) += 1;
-		match self.tx.send(AsyncSinkOp::FlushSink { sink: sink }) {
+		let Some(ref tx) = self.tx else {
+			return;
+		};
+		self.inc_queue_size();
+
+		match tx.send(AsyncSinkOp::FlushSink { sink: sink }) {
 			Ok(_) => (),
 			Err(e) => panic!("failed to queue sink flush: {e}"),
 		};
@@ -95,12 +107,12 @@ impl AsyncSinkHandler {
 
 	fn flush_queue(&self) {
 		let start = Timestamp::now();
-		while self.queue_size() != 0 {
+		while self.get_queue_size() != 0 {
 			if Timestamp::now().diff_as_duration(&start) > ASYNC_HANDLER_OP_TIMEOUT {
 				panic!(
 					"failed to flush AsyncSinkHanlder after {wait:?}, {size} ops left",
 					wait = ASYNC_HANDLER_OP_TIMEOUT,
-					size = self.queue_size()
+					size = self.get_queue_size()
 				);
 			}
 			sleep(ASYNC_HANDLER_SPINLOCK_WAIT);
@@ -119,6 +131,8 @@ impl Drop for AsyncSinkHandler {
 	fn drop(&mut self) {
 		self.flush_queue();
 
+		// close the async queue sender and wait for the handler thread to die
+		self.tx = None;
 		if let Some(rx_handler) = self.rx_handler.take() {
 			match rx_handler.join() {
 				Ok(_) => (),
@@ -128,19 +142,21 @@ impl Drop for AsyncSinkHandler {
 	}
 }
 
-// TODO: fix me
 fn drop() {
-	//*(GLOBAL_ASYNC_HANDLER.lock().unwrap()) = None;
+	*(GLOBAL_ASYNC_HANDLER.lock().unwrap()) = None;
 }
 
+/// Returns the number of active loggers referencing the global async handler.
 pub fn refcount() -> u32 {
 	*(GLOBAL_ASYNC_HANDLER_REFCOUNT.lock().unwrap())
 }
 
+/// Increments the count of active loggers referencing the global async handler.
 pub fn inc_refcount() {
 	*(GLOBAL_ASYNC_HANDLER_REFCOUNT.lock().unwrap()) += 1;
 }
 
+/// Decrements the count of active loggers referencing the global async handler.
 pub fn dec_refcount() {
 	let mut count = GLOBAL_ASYNC_HANDLER_REFCOUNT.lock().unwrap();
 	if *count == 0 {
@@ -149,20 +165,20 @@ pub fn dec_refcount() {
 	*count -= 1;
 
 	if *count == 0 {
-		// shutdown handler if no clients are left
+		// shutdown handler once no loggers are referencing the async queue
 		drop();
 	}
 }
 
-/*
+/// Returns the async queue size.
 pub fn size() -> usize {
 	match GLOBAL_ASYNC_HANDLER.lock().unwrap().as_ref() {
-		Some(h) => h.queue_size(),
+		Some(h) => h.get_queue_size(),
 		None => 0,
 	}
 }
-*/
 
+/// Flushes all pending async queue operations, locking until completion.
 pub fn flush() {
 	match GLOBAL_ASYNC_HANDLER.lock().unwrap().as_ref() {
 		Some(h) => h.flush_queue(),
@@ -170,10 +186,12 @@ pub fn flush() {
 	};
 }
 
+/// Queues a log operation on the async queue.
 pub fn log(sink: &SinkRef, update: &LogUpdate, attrs: &attributes::Map) {
 	GLOBAL_ASYNC_HANDLER.lock().unwrap().get_or_insert_default().log(sink.clone(), update.clone(), attrs.clone())
 }
 
+/// Queues a sink flush operation on the async queue.
 pub fn flush_sink(sink: &SinkRef) {
 	GLOBAL_ASYNC_HANDLER.lock().unwrap().get_or_insert_default().flush_sink(sink.clone())
 }
