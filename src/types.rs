9804f6458d4337e0dafbd9035fd7fc2477ef9738
Lisandro Pin <lisandrop@proton.me>
2026-04-19 00:44:48 +0200

    Implement a basic PRNG for sampling filter operations.

--- src/types.rs
+++ src/types.rs
@@ -1,3 +1,4 @@
+use ntime::Timestamp;
 use std::str;
 use std::sync::Arc;
 use std::sync::Mutex;
@@ -54,10 +55,36 @@ impl ShortString {
 	}
 }
 
+/// Rand is a 64-bit PRNG, implementing the Xorshift algorithm (https://en.wikipedia.org/wiki/Xorshift),
+/// with a period of 2^64−1.
+pub struct Rand {
+	state: u64,
+}
+
+impl Rand {
+	pub fn with_seed(seed: u64) -> Self {
+		Self { state: seed }
+	}
+
+	pub fn new() -> Self {
+		Self {
+			state: Timestamp::now().as_nanos() as u64,
+		}
+	}
+
+	pub fn next(&mut self) -> u64 {
+		self.state ^= self.state << 13;
+		self.state ^= self.state >> 7;
+		self.state ^= self.state << 17;
+
+		self.state
+	}
+}
+
 /* ----------------------- Tests ----------------------- */
 
 #[cfg(test)]
-mod basic_tests {
+mod short_string {
 	use super::*;
 
 	#[test]
@@ -83,5 +110,31 @@ mod basic_tests {
 
 		assert_eq!(ss.as_str(), s);
 		assert_eq!(ss.len(), SHORT_STRING_MAX_SIZE)
+	}
+}
+
+#[cfg(test)]
+mod rand {
+	use super::*;
+	use std::array;
+
+	#[test]
+	fn generation() {
+		let mut rand = Rand::with_seed(12345678);
+		let got: [u64; 10] = array::from_fn(|_| rand.next());
+		let want: [u64; 10] = [
+			0x002f470eb7948a0c,
+			0xf0a0b1ee9ea8a018,
+			0x56afa382130ef758,
+			0x1fda4adc73123cb6,
+			0xd84256eca273f54f,
+			0x69f5cfe0dba9a165,
+			0x5fd04f88d2940b67,
+			0x786ed9cfd23d1ab1,
+			0xb98f4edcb801ecc4,
+			0xb02a094b80a85e1d,
+		];
+
+		assert_eq!(got, want);
 	}
 }
