plisandro/rasant

commit ac5711f6a2af2594d44df4871121bc9bec33684d

Fix string escaping for JSON and text output formats.

src/format/compact.rs
@@ -22,8 +22,8 @@ pub fn default_format_config() -> FormatterConfig {
 pub fn write_scalar<T: io::Write>(out: &mut T, s: &Scalar) -> io::Result<()> {
 	match s {
 		Scalar::Bool(b) => write!(out, "{}", b),
-		Scalar::ShortString(ss) => write!(out, "\"{}\"", ss.as_str()),
-		Scalar::String(s) => write!(out, "\"{}\"", s),
+		Scalar::ShortString(ss) => write!(out, "\"{}\"", ss.as_str().escape_default()),
+		Scalar::String(s) => write!(out, "\"{}\"", s.escape_default()),
 		Scalar::Int(i) => write!(out, "{}", i),
 		Scalar::LongInt(i) => {
 			if *i < 1 {
@@ -105,6 +105,7 @@ mod tests {
 			(Scalar::Bool(true), "true"),
 			(Scalar::String("".into()), "\"\""),
 			(Scalar::String("abcd 1234".into()), "\"abcd 1234\""),
+			(Scalar::String("quizás\n\"lala\"".into()), "\"quiz\\u{e1}s\\n\\\"lala\\\"\""),
 			(Scalar::Int(-123), "-123"),
 			(Scalar::LongInt(-12345678901234567), "-0x2bdc545d6b4b87"),
 			(Scalar::Size(89801234567890123), "0x13f09bf3ecf84cb"),

src/format/json.rs
@@ -23,8 +23,8 @@ pub fn default_format_config() -> FormatterConfig {
 pub fn write_scalar<T: io::Write>(out: &mut T, s: &Scalar) -> io::Result<()> {
 	match s {
 		Scalar::Bool(b) => write!(out, "{}", b),
-		Scalar::ShortString(ss) => write!(out, "\"{}\"", ss.as_str()),
-		Scalar::String(s) => write!(out, "\"{}\"", s),
+		Scalar::ShortString(ss) => write!(out, "\"{}\"", ss.as_str().escape_default()),
+		Scalar::String(s) => write!(out, "\"{}\"", s.escape_default()),
 		Scalar::Int(i) => write!(out, "{}", i),
 		Scalar::LongInt(i) => write!(out, "{}", i),
 		Scalar::Size(s) => write!(out, "{}", s),
@@ -115,6 +115,7 @@ mod tests {
 			(Scalar::Bool(true), "true"),
 			(Scalar::String("".into()), "\"\""),
 			(Scalar::String("abcd 1234".into()), "\"abcd 1234\""),
+			(Scalar::String("quizás\n\"lala\"".into()), "\"quiz\\u{e1}s\\n\\\"lala\\\"\""),
 			(Scalar::Int(-123), "-123"),
 			(Scalar::LongInt(-12345678901234567), "-12345678901234567"),
 			(Scalar::LongInt(89801234567890123), "89801234567890123"),

commit 1588b0482968b130c156600c55a2779192cb9ec4

Introduce an unified `types::AttributeString` for all string types supported by scalars.
Also adds logic to optimize escaped string output, fixing a performance
regression introduced by ac5711f6.

src/attributes/scalar.rs
@@ -3,7 +3,7 @@ use std::fmt;
 use std::fmt::Write;
 use std::thread;
 
-use crate::types;
+use crate::types::AttributeString;
 
 /// [`Scalar`] definitions for all log operations.
 /// Scalars are the basic data units for Rasant, representing a single type.
@@ -11,10 +11,8 @@ use crate::types;
 pub enum Scalar {
 	/// A [`bool`]ean.
 	Bool(bool),
-	/// An owned short string, akin to [`&str`].
-	ShortString(types::ShortString),
-	/// An owned [`String`], using heap storage.
-	String(String),
+	/// An owned [`AttributeString`].
+	String(AttributeString),
 	/// An integer, internally stored as a [`i64`].
 	Int(i64),
 	/// A long integer, internally stored as a [`i128`].
@@ -37,8 +35,7 @@ impl fmt::Display for Scalar {
 	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
 		match &self {
 			Self::Bool(b) => write!(f, "{}", b),
-			Self::ShortString(ss) => write!(f, "\"{}\"", ss.as_str()),
-			Self::String(s) => write!(f, "\"{}\"", s),
+			Self::String(s) => write!(f, "\"{}\"", s.as_str()),
 			Self::Int(i) => write!(f, "{}", i),
 			Self::LongInt(i) => {
 				if *i < 1 {
@@ -104,21 +101,16 @@ impl ToScalar for bool {
 	}
 }
 
+// TODO: optimize me
 impl ToScalar for String {
 	fn to_scalar(&self) -> Scalar {
-		match types::ShortString::from(self) {
-			Ok(ss) => Scalar::ShortString(ss),
-			Err(_) => Scalar::String(self.clone()),
-		}
+		Scalar::String(AttributeString::from(self.clone()))
 	}
 }
 
-impl ToScalar for &str {
+impl ToScalar for &'static str {
 	fn to_scalar(&self) -> Scalar {
-		match types::ShortString::from(*self) {
-			Ok(ss) => Scalar::ShortString(ss),
-			Err(_) => Scalar::String((*self).into()),
-		}
+		Scalar::String(AttributeString::from(*self))
 	}
 }
 
@@ -146,17 +138,15 @@ impl ToScalar for &Timestamp {
 	}
 }
 
-// TODO: Switch to ShortString
 impl ToScalar for thread::ThreadId {
 	fn to_scalar(&self) -> Scalar {
-		Scalar::String(format!("{:?}", *self))
+		Scalar::String(AttributeString::from(format!("{:?}", *self)))
 	}
 }
 
-// TODO: Switch to ShortString
 impl ToScalar for &thread::ThreadId {
 	fn to_scalar(&self) -> Scalar {
-		Scalar::String(format!("{:?}", *self))
+		Scalar::String(AttributeString::from(format!("{:?}", *self)))
 	}
 }
 
@@ -268,14 +258,8 @@ mod tests {
 		let long_string = "this is a rather long string, which may be complicated";
 
 		assert_eq!(true.to_scalar(), Scalar::Bool(true));
-		assert_eq!(
-			short_string.to_scalar(),
-			Scalar::ShortString(types::ShortString::from(short_string).expect("ShortString serialization failed"))
-		);
-		assert_eq!(
-			String::from(short_string).to_scalar(),
-			Scalar::ShortString(types::ShortString::from(short_string).expect("ShortString serialization failed"))
-		);
+		assert_eq!(short_string.to_scalar(), Scalar::String(AttributeString::from(short_string)));
+		assert_eq!(String::from(short_string).to_scalar(), Scalar::String(AttributeString::from(short_string)));
 		assert_eq!(long_string.to_scalar(), Scalar::String(long_string.into()));
 		assert_eq!(String::from(long_string).to_scalar(), Scalar::String(long_string.into()));
 		assert_eq!((-12 as i8).to_scalar(), Scalar::Int(-12));

src/attributes/value.rs
@@ -146,7 +146,7 @@ impl<'i, T: ToScalar, const N: usize> ToValue for [T; N] {
 mod tests {
 	use super::*;
 
-	use crate::types;
+	use crate::types::AttributeString;
 	use ntime::{Duration, Timestamp};
 
 	#[test]
@@ -155,14 +155,8 @@ mod tests {
 		let long_string = "this is a rather long string, which may be complicated";
 
 		assert_eq!(true.to_value(), Value::Scalar(Scalar::Bool(true)));
-		assert_eq!(
-			short_string.to_value(),
-			Value::Scalar(Scalar::ShortString(types::ShortString::from(short_string).expect("ShortString serialization failed")))
-		);
-		assert_eq!(
-			String::from(short_string).to_value(),
-			Value::Scalar(Scalar::ShortString(types::ShortString::from(short_string).expect("ShortString serialization failed")))
-		);
+		assert_eq!(short_string.to_value(), Value::Scalar(Scalar::String(AttributeString::from(short_string))));
+		assert_eq!(String::from(short_string).to_value(), Value::Scalar(Scalar::String(AttributeString::from(short_string))));
 		assert_eq!(long_string.to_value(), Value::Scalar(Scalar::String(long_string.into())));
 		assert_eq!(String::from(long_string).to_value(), Value::Scalar(Scalar::String(long_string.into())));
 		assert_eq!((-12 as i8).to_value(), Value::Scalar(Scalar::Int(-12)));
@@ -221,8 +215,7 @@ mod tests {
 	#[test]
 	fn dbg_format() {
 		assert_eq!(format!("{}", Value::Scalar(Scalar::Bool(true))), "true");
-		assert_eq!(format!("{}", Value::Scalar(Scalar::String("boo".into()))), "\"boo\"");
-		assert_eq!(format!("{}", Value::Scalar(Scalar::ShortString(types::ShortString::from("abcd 1234").unwrap()))), "\"abcd 1234\"");
+		assert_eq!(format!("{}", Value::Scalar(Scalar::String(AttributeString::from("boo")))), "\"boo\"");
 		assert_eq!(format!("{}", Value::Scalar(Scalar::Size(-12345678901234567))), "-0x2bdc545d6b4b87");
 		assert_eq!(format!("{}", Value::Scalar(Scalar::Uint(123456))), "123456");
 		assert_eq!(
@@ -231,7 +224,7 @@ mod tests {
 				Value::List(&[
 					Scalar::Bool(true),
 					Scalar::String("boo".into()),
-					Scalar::ShortString(types::ShortString::from("abcd 1234").unwrap()),
+					Scalar::String(AttributeString::from("abcd 1234")),
 					Scalar::Size(-12345678901234567),
 					Scalar::Uint(123456),
 				])

src/format/compact.rs
@@ -22,8 +22,7 @@ pub fn default_format_config() -> FormatterConfig {
 pub fn write_scalar<T: io::Write>(out: &mut T, s: &Scalar) -> io::Result<()> {
 	match s {
 		Scalar::Bool(b) => write!(out, "{}", b),
-		Scalar::ShortString(ss) => write!(out, "\"{}\"", ss.as_str().escape_default()),
-		Scalar::String(s) => write!(out, "\"{}\"", s.escape_default()),
+		Scalar::String(s) => s.write_quoted_escaped(out),
 		Scalar::Int(i) => write!(out, "{}", i),
 		Scalar::LongInt(i) => {
 			if *i < 1 {

src/format/json.rs
@@ -23,8 +23,7 @@ pub fn default_format_config() -> FormatterConfig {
 pub fn write_scalar<T: io::Write>(out: &mut T, s: &Scalar) -> io::Result<()> {
 	match s {
 		Scalar::Bool(b) => write!(out, "{}", b),
-		Scalar::ShortString(ss) => write!(out, "\"{}\"", ss.as_str().escape_default()),
-		Scalar::String(s) => write!(out, "\"{}\"", s.escape_default()),
+		Scalar::String(s) => s.write_quoted_escaped(out),
 		Scalar::Int(i) => write!(out, "{}", i),
 		Scalar::LongInt(i) => write!(out, "{}", i),
 		Scalar::Size(s) => write!(out, "{}", s),

src/lib.rs
@@ -284,3 +284,4 @@ pub use level::Level;
 pub use logger::Logger;
 /// [`ntime::Format`], re-exported for convenience.
 pub use ntime::Format as TimeFormat;
+pub use types::AttributeString;

src/types.rs
@@ -1,4 +1,5 @@
 use ntime::Timestamp;
+use std::io;
 use std::str;
 use std::sync::Arc;
 use std::sync::Mutex;
@@ -18,55 +19,128 @@ pub type SinkRef = Arc<Mutex<Box<dyn Sink + Send>>>;
 /// A sender channel for [`AsyncSinkOp`] async log operations.
 pub type AsyncSinkSender = mpsc::Sender<AsyncSinkOp>;
 
-/// ShortString is a string stored in a (small) fixed-size buffer, to avoid heap allocations.
+/// AttributeString is a container for all string types supported as attibutes.
 #[derive(Clone, Debug)]
-pub struct ShortString {
+pub struct AttributeString {
+	static_buf: Option<&'static str>,
+	heap_string: Option<String>,
 	buf: [u8; SHORT_STRING_MAX_SIZE],
-	size: usize,
+	buf_size: usize,
+	needs_escaping: bool,
 }
 
-impl ShortString {
-	/// Initializes a [`ShortString`] from a [`&str`].
-	pub fn from(s: &str) -> Result<Self, &str> {
-		if s.len() > SHORT_STRING_MAX_SIZE {
-			return Err("max string size exceeded");
-		}
-
-		let mut res = Self {
+impl From<&'static str> for AttributeString {
+	fn from(s: &'static str) -> Self {
+		Self {
+			static_buf: Some(s),
+			heap_string: None,
 			buf: [0; SHORT_STRING_MAX_SIZE],
-			size: s.len(),
-		};
-		for i in 0..res.size {
-			res.buf[i] = s.as_bytes()[i];
+			buf_size: 0,
+			needs_escaping: AttributeString::has_escapable_chars(s),
 		}
-
-		Ok(res)
-	}
-
-	/// Returns the byte size for this [`ShortString`].
-	pub fn len(&self) -> usize {
-		self.size
-	}
-
-	/// Returns a [`&str`] slice for this [`ShortString`].
-	pub fn as_str(&self) -> &str {
-		str::from_utf8(&self.buf[0..self.size]).expect("failed to deserialize ShortString")
 	}
 }
 
-impl PartialEq for ShortString {
-	fn eq(&self, other: &Self) -> bool {
-		if self.size != other.size {
-			return false;
+impl From<String> for AttributeString {
+	fn from(s: String) -> Self {
+		let needs_escaping = AttributeString::has_escapable_chars(s.as_str());
+
+		if s.len() <= SHORT_STRING_MAX_SIZE {
+			// we can store this string locally \o/
+			let mut res = Self {
+				static_buf: None,
+				heap_string: None,
+				buf: [0; SHORT_STRING_MAX_SIZE],
+				buf_size: s.len(),
+				needs_escaping: needs_escaping,
+			};
+			for i in 0..res.buf_size {
+				res.buf[i] = s.as_bytes()[i];
+			}
+
+			return res;
 		}
 
-		for i in 0..self.size {
-			if self.buf[i] != other.buf[i] {
-				return false;
+		Self {
+			static_buf: None,
+			heap_string: Some(s),
+			buf: [0; SHORT_STRING_MAX_SIZE],
+			buf_size: 0,
+			needs_escaping: needs_escaping,
+		}
+	}
+}
+
+impl<'i> AttributeString {
+	/// Evaluates whether a given [`&str`] contains escapable characters.
+	fn has_escapable_chars(s: &str) -> bool {
+		// this is horrible, alas...
+		let mut escaped_iter = s.escape_default();
+		for c in s.chars() {
+			match escaped_iter.next() {
+				None => return true, // oops
+				Some(ec) => {
+					if c != ec {
+						return true;
+					}
+				}
 			}
 		}
+		false
+	}
 
-		true
+	/// Returns a binary length for this [`AttributeString`].
+	pub fn len(&self) -> usize {
+		if let Some(s) = self.static_buf {
+			return s.len();
+		}
+		if let Some(s) = &self.heap_string {
+			return s.len();
+		}
+		return self.buf_size;
+	}
+
+	/// Returns a [`&str`] slice for this [`AttributeString`].
+	pub fn as_str(&self) -> &str {
+		if let Some(s) = self.static_buf {
+			return s;
+		}
+		if let Some(s) = &self.heap_string {
+			return s.as_str();
+		}
+		str::from_utf8(&self.buf[0..self.buf_size]).expect("failed to deserialize AttributeString buffer")
+	}
+
+	/// lalala
+	pub fn write<T: io::Write>(&self, out: &mut T) -> io::Result<()> {
+		write!(out, "{}", self.as_str())
+	}
+
+	/// lalala
+	pub fn write_escaped<T: io::Write>(&self, out: &mut T) -> io::Result<()> {
+		match self.needs_escaping {
+			false => write!(out, "{}", self.as_str()),
+			true => write!(out, "{}", self.as_str().escape_default()),
+		}
+	}
+
+	/// lalala
+	pub fn write_quoted<T: io::Write>(&self, out: &mut T) -> io::Result<()> {
+		write!(out, "\"{}\"", self.as_str())
+	}
+
+	/// lalala
+	pub fn write_quoted_escaped<T: io::Write>(&self, out: &mut T) -> io::Result<()> {
+		match self.needs_escaping {
+			false => write!(out, "\"{}\"", self.as_str()),
+			true => write!(out, "\"{}\"", self.as_str().escape_default()),
+		}
+	}
+}
+
+impl PartialEq for AttributeString {
+	fn eq(&self, other: &Self) -> bool {
+		self.as_str() == other.as_str()
 	}
 }
 
@@ -98,6 +172,7 @@ impl Rand {
 
 /* ----------------------- Tests ----------------------- */
 
+/*
 #[cfg(test)]
 mod short_string {
 	use super::*;
@@ -127,6 +202,7 @@ mod short_string {
 		assert_eq!(ss.len(), SHORT_STRING_MAX_SIZE)
 	}
 }
+*/
 
 #[cfg(test)]
 mod rand {